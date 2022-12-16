ed6b5dc70fbaa9d4d1f6a723390cafc0d515753d
Julian Schule <julianschule1@Julians-MacBook-Air.local>
2022-12-16 12:05:56 +0100

    css

---
+++ shop/script.js
@@ -0,0 +1,13 @@
+const { body } = document;
+
+async function itemToHTML(iName) {
+        let response = await fetch(`./items/${iName}/${iName}.json`, {cache: "reload"})
+        let item = await response.json()
+        let html = `<a><img id="image" src="items/${iName}/${item.img}" alt="${item.name}" width="50px" height="50px"></a><a>${item.name}</a><a>${item.price}&#8364</a><br>`
+        let child = document.createElement('li');
+        child.innerHTML = html;
+        child.id = "item-container"
+        body.querySelector("main").querySelector("ul").appendChild(child)
+}
+await itemToHTML("apple")
+await itemToHTML("banana")
